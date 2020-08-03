0b8b8a56b3337dbee27a7deb1d10fe83fc8caabc
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-08-03 14:35:10 +0200

    WIP: Trusted grub rootless
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- stboot/mixed-firmware/make_image.sh
+++ stboot/mixed-firmware/make_image.sh
@@ -56,14 +56,48 @@ mkfs.vfat -C -n "STBOOT" "${img}".vfat $((size_vfat >> 10))
 echo "[INFO]: Installing Trusted Grub"
 mmd -i "${img}".vfat ::boot
 mmd -i "${img}".vfat ::boot/grub
+mmd -i "${img}".vfat ::boot/grub/i386-pc
+mmd -i "${img}".vfat ::boot/grub/themes
+mmd -i "${img}".vfat ::boot/grub/themes/starfield
+mmd -i "${img}".vfat ::boot/grub/fonts
 
 mkdir -p "${src}/grub2"
 echo "configfile (hd0,msdos1)/boot/grub/grub.cfg" > "${src}/grub2/grub-early.cfg"
 echo "(hd0) /dev/loop0" > "${src}/grub2/device.map"
 echo "(hostdisk/dev/loop0) ${img}.vfat" >> "${src}/grub2/device.map"
 echo "
+debug
+
+serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
+terminal_input --append  serial
+terminal_output --append serial
+set timeout=1
+play 480 440 1
+
 insmod ext2
 insmod gettext
+insmod font
+insmod fat
+insmod part_gpt
+insmod part_msdos
+
+if [ \"\${grub_platform}\" == \"efi\" ]; then
+	insmod efi_gop
+	insmod efi_uga
+fi
+
+if [ \"\${grub_platform}\" == \"pc\" ]; then
+	insmod vbe
+	insmod vga
+fi
+
+if loadfont \${prefix}/fonts/unicode.pf2
+then
+    insmod gfxterm
+    set gfxmode=auto
+    set gfxpayload=keep
+    terminal_output console
+fi
 
 set default='0'
 set timeout='3'
@@ -71,14 +105,32 @@ set root='(h0,1)'
 
 menuentry \"STBOOT\" {
 	linux /boot/${lnxbt_kernel}
-   initrd /boot/${lnxbt_initramfs}
+	initrd /boot/${lnxbt_initramfs}
 }" > "${src}/grub2/grub.cfg"
 
-"${src}/bin/grub-mkimage" -O i386-pc -c "${src}/grub2/grub-early.cfg" -o "${src}/grub2/core.img" -p '(hd0,msdos1)/boot/grub' biosdisk boot chain configfile ext2 linux ls part_msdos reboot serial vga gettext
+"${src}/bin/grub-mkimage" -O i386-pc -c "${src}/grub2/grub-early.cfg" -o "${src}/grub2/core.img" -p '(hd0,msdos1)/boot/grub' search_fs_uuid fat biosdisk boot chain configfile ext2 linux ls part_msdos reboot serial vga gettext
 
-cp "${src}/lib/grub/i386-pc"/*.img "${src}/grub2/"
 mcopy -i "${img}".vfat "${src}/grub2/core.img" ::boot/grub/grub
+grub_offset=$(LANG=C grep --max-count=1 --only-matching --byte-offset --binary --text --perl-regexp "\x52\x56\xbe\x63\x81\xe8\x8a\x01" "${img}".vfat|cut -d: -f1)
+
+for f in "${src}/lib/grub/i386-pc"/*.mod; do
+	mcopy -i "${img}".vfat "${f}" ::boot/grub/i386-pc/
+done
+for f in "${src}/lib/grub/i386-pc"/*.o; do
+        mcopy -i "${img}".vfat "${f}" ::boot/grub/i386-pc/
+done
+for f in "${src}/lib/grub/i386-pc"/*.lst; do
+        mcopy -i "${img}".vfat "${f}" ::boot/grub/i386-pc/
+done
+for f in "${src}/lib/grub/i386-pc"/*.sh; do
+        mcopy -i "${img}".vfat "${f}" ::boot/grub/i386-pc/
+done
+for f in "${src}/share/grub/themes/starfield"/*; do
+        mcopy -i "${img}".vfat "${f}" ::boot/grub/themes/starfield/
+done
+mcopy -i "${img}".vfat "${src}/grub2/core.img" ::boot/grub/i386-pc/core.img
 mcopy -i "${img}".vfat "${src}/grub2/grub.cfg" ::boot/grub/grub.cfg
+mcopy -i "${img}".vfat "${src}/share/grub/unicode.pf2" ::boot/grub/fonts/unicode.pf2
 
 echo "[INFO]: Moving linuxboot kernel and initramfs to image"
 mcopy -i "${img}".vfat "${lnxbt_kernel}" ::
@@ -138,6 +190,41 @@ parted -s --align optimal "${img}" mklabel gpt mkpart "STBOOT" fat32 "$((offset_
 echo ""
 echo "[INFO]: Installing MBR"
 dd bs=440 count=1 conv=notrunc if="${src}/grub2/boot.img" of="${img}" status=none
+# Set GRUB_BOOT_MACHINE_BOOT_DRIVE to 0xff
+echo -n -e '\xFF'| dd of="${img}" seek=$((0x64)) count=1 bs=1 conv=notrunc
+# Set GRUB_BOOT_MACHINE_KERNEL_SECTOR to VFAT offset + file offset
+n=$((${offset_vfat} + (${grub_offset}/512)))
+echo "GRUB2 core.img is at $((${offset_vfat} * 512 + ${grub_offset})) sector $n"
+for i in 0 1 2 3 4 5 6 7; do
+	printf \\$(printf '%03o' $((($n >> ($i * 8)) & 0xff))) | dd of="${img}" seek=$((0x5c + $i)) count=1 bs=1 conv=notrunc
+done
+
+#mkdir -p mnt
+
+#echo "IMG ${img}"
+#sudo kpartx -v -a "${img}"
+#sudo losetup /dev/loop1 /dev/mapper/loop1p1
+#sudo mount /dev/loop2 ./mnt
+
+#sudo mkdir -p ./mnt/dev
+#sudo mount --bind /dev ./mnt/dev
+#sudo mkdir -p ./mnt/boot/grub
+#sudo cat > ./mnt/boot/grub/device.map <<EOF
+#(hd0)   /dev/loop2
+#(hd0,1) /dev/loop3
+#EOF
+
+#sudo mkdir -p ./mnt/sbin
+#sudo cp "${src}/sbin/grub-mkconfig" ./mnt/sbin
+
+#sudo chroot ./mnt /sbin/grub-mkconfig -o /boot/grub/grub.cfg
+#"${src}/sbin/grub-install" --no-floppy --grub-mkdevicemap=./mnt/boot/grub/device.map --root-directory=./mnt /dev/loop1
+
+#sudo umount ./mnt/dev
+#sudo umount ./mnt
+#sudo losetup -d /dev/loop2
+#sudo kpartx -v -d /dev/loop1
+
 
 echo ""
 echo "[INFO]: Image layout:"
